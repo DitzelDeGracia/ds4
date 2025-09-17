e48167b5a17327257f63fd5fee7b1b149ed9b733
Ditzel De Gracia <Ditzel De Gracia@DESKTOP-T5ELMVO>
2025-09-16 23:39:42 -0500

    Entrega del Laboratorio2

---
+++ Laboratorio2/Laboratorio2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Laboratorio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Client client = new Client();
+            client.FirstName = "Ditzel";
+            client.LastName = "De Gracia";
+            client.Age = 23;
+            client.Id = 1;
+
+            Console.WriteLine(client.GetFullName());
+
+        }
+
+    }
+
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ushort Age { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+
+        }
+    }
+}
---
+++ Laboratorio2/Laboratorio22/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Laboratorio21
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            MyClass.Valor = 1;
+            Console.WriteLine(MyClass.Valor);
+        }
+    }
+
+    public class MyClass
+    {
+        public static int Valor;
+    }
+}
